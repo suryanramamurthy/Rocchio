acc60c7dc845de20aaa5c3530a10eaf9668712ab
Suryanarayanan Ramamurthy <aparna.suryan@gmail.com>
2019-04-29 12:12:53 +0530

    Add files via upload

---
+++ DFProject/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Accord.IO;
+using Accord.Math;
+using Accord.MachineLearning;
+
+namespace DFProject
+{
+    class Class1
+    {
+        LinearCoordinateDescent 
+
+    }
+
+}
